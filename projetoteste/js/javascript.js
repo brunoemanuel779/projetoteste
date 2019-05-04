3a1e30dc3158af5c295bfafc153c1027aedab635
Bruno <brunoemanuel779@gmail.com>
2019-05-04 17:04:27 -0300

    importacao inicial

---
+++ projetoteste/js/javascript.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    
+	$(".menumobile").click(function(){
+       $(".menur").css({'display':'block'});
+        $(".linkm").css({'display':'inline'});
+        
+       
+        
+    });
+ });
